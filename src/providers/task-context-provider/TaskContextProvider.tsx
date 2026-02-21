16e5b3f797af3827040bc786a2b665f392f3661a
Jean Charles Guichard Guichard <jeancharles.g.guichard@gmail.com>
2026-02-21 14:52:04 -0300

    feat: :sparkles: Adds audio when finish task and change favicon

--- src/providers/task-context-provider/TaskContextProvider.tsx
+++ src/providers/task-context-provider/TaskContextProvider.tsx
@@ -1,8 +1,9 @@
-import { useEffect, useReducer } from "react";
+import { useEffect, useReducer, useRef } from "react";
 import { TaskContext } from "../../contexts/task-context/TaskContext";
 import { initialTaskState } from "../../constants/task-constants/initialTaskState";
 import { taskReducer } from "../../reducers/taskReducer";
 import TimeWorkerManager from "../../workers/TimeWorkerManager";
+import { loadBeep } from "../../utils/loadBeep";
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
@@ -10,12 +11,18 @@ type TaskContextProviderProps = {
 
 export const TaskContextProvider = ({ children }: TaskContextProviderProps) => {
   const [state, dispatch] = useReducer(taskReducer, initialTaskState);
+  const playBeepRef = useRef<ReturnType<typeof loadBeep> | null>(null);
+
   const worker = TimeWorkerManager.getInstance();
 
   worker.onmessage((message) => {
     const countDownSeconds = message.data;
 
     if (countDownSeconds <= 0) {
+      if (playBeepRef.current) {
+        playBeepRef.current();
+        playBeepRef.current = null;
+      }
       dispatch({ type: "COMPLETE_TASK" });
       worker.terminate();
     } else {
@@ -34,6 +41,14 @@ export const TaskContextProvider = ({ children }: TaskContextProviderProps) => {
 
     worker.postMessage(state);
   }, [state, worker]);
+
+  useEffect(() => {
+    if (state.activeTask && playBeepRef.current === null) {
+      playBeepRef.current = loadBeep();
+    } else {
+      playBeepRef.current = null;
+    }
+  }, [state.activeTask]);
 
   return (
     <TaskContext.Provider value={{ state, dispatch }}>
